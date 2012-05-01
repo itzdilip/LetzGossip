7ea3dd47f21e0ee91306b5f01fe7771184726355
Dilip S M <itzdilip@hotmail.com>
2012-05-01 12:01:55 +0530

    Project Structure

---
+++ LetzGossip/src/test/java/com/mljava/TestDeviceInformationService.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.mljava;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * @author dilip
+ *
+ */
+public class TestDeviceInformationService {
+
+	@Test
+	public void test() {
+		Assert.assertEquals("", "");
+	}
+
+}
