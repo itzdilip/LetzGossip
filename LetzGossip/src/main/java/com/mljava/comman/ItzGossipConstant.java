1896d2111e0f561c311d9f6cf486233ed88ddd74
Dilip S M <itzdilip@hotmail.com>
2012-05-01 12:16:45 +0530

    Sample Project Struture With Gossip Constnt and Properties files

---
+++ LetzGossip/src/main/java/com/mljava/comman/ItzGossipConstant.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.mljava.comman;
+
+/**
+ * @author dilip
+ *
+ */
+public final class ItzGossipConstant {
+	
+	public static final String EMPTY_STRING="";
+	public static final String VALUE_NOT_SET="VALUE NOT SET";
+
+}
